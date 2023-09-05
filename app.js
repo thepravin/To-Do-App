1835449a811da50e65370036437ef8bdfd3bff75
Pravin <thepravin28@gmail.com>
2023-09-05 13:59:24 +0530

    create crros icon

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-
-function addTask(){
-    if(inputBox.value==''){
-        alert("You Must Write Something");
-    }else{
-        let li = document.createElement("li");
-        li.innerText=inputBox.value;
-        listContainer.appendChild(li);
-    }
+function addTask() {
+  if (inputBox.value == "") {
+    alert("You Must Write Something");
+  } else {
+    let li = document.createElement("li");
+    li.innerText = inputBox.value;
+    listContainer.appendChild(li);
+    // add crros icon
+    let span = document.createElement("span");
+    span.innerText="\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = " ";  // errase box context
 }
